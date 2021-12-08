9e1dff6eb04613bc627e546c0d145dae80c6ad3b
Opilane TTHK <opilane@tthk.ee>
2021-12-08 14:37:21 +0200

    Add 404, getdate and getweekday

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require('express');
+const date = require(__dirname+'/getDate.js');
 
 const app = express();
+app.use(express.static('public'));
 
 app.get('/',(request, response)=> {
     response.send('hello, world!');
 });
 
+app.get('/getdate', (req, res) => { //req - request, res - response
+    let today = date.getDate();
+    res.send(today);
+});
 
+app.get('/getweekday', (req, res) => { //req - request, res - response
+    let today = date.getWeekDay();
+    res.send(today);
+});
 
+app.get('*', (req, res) => {
+    res.sendFile(__dirname+"/404.html");
+});
 
 port = 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}.`)
+    console.log(`Server is running on port ${port}.`);
 });
